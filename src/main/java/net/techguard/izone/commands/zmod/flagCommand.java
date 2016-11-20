699b8635971cccb1462d2f13297d808a8f3cf1b5
TryHardDood <rsnconfigs@gmail.com>
2016-11-20 22:20:08 +0100

    Fixed teleport bug

--- src/main/java/net/techguard/izone/commands/zmod/flagCommand.java
+++ src/main/java/net/techguard/izone/commands/zmod/flagCommand.java
@@ -13,6 +13,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 import static net.techguard.izone.Phrases.phrase;
 
@@ -167,6 +168,13 @@ public class flagCommand extends zmodBase {
 					if(flag2 == Flags.TELEPORT)
 					{
 						Location location = player.getLocation();
+
+						Border border = new Border(zone.getBorder1().toVector(), zone.getBorder2().toVector());
+						if(!border.contains(player.getLocation())) {
+							player.sendMessage(iZone.getPrefix() + ChatColor.RED + "This location is not in your zone.");
+							return;
+						}
+
 						if(isSafeLocation(location))
 						{
 							zone.setTeleport(location);
@@ -308,5 +316,34 @@ public class flagCommand extends zmodBase {
 			return false;
 		}
 		return true;
+	}
+
+	public class Border {
+
+		private Vector p1;
+		private Vector p2;
+
+
+		public Border(Vector p1, Vector p2) {
+			int x1 = Math.min(p1.getBlockX(), p2.getBlockX());
+			int y1 = Math.min(p1.getBlockY(), p2.getBlockY());
+			int z1 = Math.min(p1.getBlockZ(), p2.getBlockZ());
+			int x2 = Math.max(p1.getBlockX(), p2.getBlockX());
+			int y2 = Math.max(p1.getBlockY(), p2.getBlockY());
+			int z2 = Math.max(p1.getBlockZ(), p2.getBlockZ());
+			this.p1 = new Vector(x1, y1, z1);
+			this.p2 = new Vector(x2, y2, z2);
+		}
+
+		public boolean contains(Location loc) {
+			if (loc == null)
+			{
+				return false;
+			}
+			return loc.getBlockX() >= p1.getBlockX() && loc.getBlockX() <= p2.getBlockX()
+			       && loc.getBlockY() >= p1.getBlockY() && loc.getBlockY() <= p2.getBlockY()
+			       && loc.getBlockZ() >= p1.getBlockZ() && loc.getBlockZ() <= p2.getBlockZ();
+		}
+
 	}
 }
